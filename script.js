5ab7b6f2381665f1c4179f5a3d487f777755aad3
AJ <faronsantoso105@gmail.com>
2025-07-02 10:12:08 +1200

    feat: sun follows path

--- script.js
+++ script.js
@@ -96,29 +96,35 @@ sunObject.addEventListener("mouseover", () => {
     sunObject.style.transform = "scale(1.1)";
 });
 sunObject.addEventListener("mouseout", () => {
-    sunObject.style.transform = "scale(1)";
+    if (!sunDragging) {
+        sunObject.style.transform = "scale(1)";
+    }
 });
 
 sunObject.addEventListener("mousedown", (event) => {
-    console.log("Mouse down")
     sunObject.style.animation = "none";
-    sunObject.style.cursor = "grabbing";
+    sunObject.style.cursor = "none";
+    document.body.style.cursor = "none";
     const sunDimensions = sunObject.getBoundingClientRect();
     sunOffsetX = event.clientX - sunDimensions.left;
     sunOffsetY = event.clientY - sunDimensions.top;
     sunDragging = true;
 });
 
-sunObject.addEventListener("mouseup", () => {
+document.addEventListener("mouseup", () => {
 	sunObject.style.animation = "hover 1s ease-in-out infinite alternate";
+    document.body.style.cursor = "default";
     sunObject.style.cursor = "grab";
     sunDragging = false;
 });
 
-sunObject.addEventListener("mousemove", (event) => {
+document.addEventListener("mousemove", (event) => {
     if (sunDragging) {
-        sunObject.style.left = `${event.clientX - sunOffsetX}px`;
-        sunObject.style.top = `${event.clientY - sunOffsetY}px`;
+        const x = event.clientX - sunOffsetX;
+        const y = (0.0015 * (x - ((window.innerWidth - sunObject.getBoundingClientRect().width)/2))**2 + 120);
+        sunObject.style.left = `${x}px`;
+        sunObject.style.top = `${y}px`;
+
     }
 });
 
